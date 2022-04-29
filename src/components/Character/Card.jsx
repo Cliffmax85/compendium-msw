6e6e20680a86216ea52544f19ea1659538596d88
cliffmax85 <cliffmax85@gmail.com>
2022-04-29 15:04:44 -0700

    did minial stlying

--- src/components/Character/Card.jsx
+++ src/components/Character/Card.jsx
@@ -1,10 +1,11 @@
-import React from 'react'
+import React from 'react';
+import styles from '../../App.css';
 
 export default function characterCard({ name, image, species, age }) {
   return (
-    <div className='char-card'>
+    <div className={styles.card}>
         <p className='name'>{name}</p>
-        <img src={image} alt='character' />
+        <img className={styles.img} src={image} alt='character' />
         <h2 className='species'>{species}</h2>
         <h2 className='age'>{age}</h2>
     </div>
