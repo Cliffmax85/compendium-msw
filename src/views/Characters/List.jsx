caff29d7e0979d194eea038a51b5bdcc9f98e140
cliffmax85 <cliffmax85@gmail.com>
2022-04-29 16:05:56 -0700

    wrote search function

--- src/views/Characters/List.jsx
+++ src/views/Characters/List.jsx
@@ -4,16 +4,31 @@ import Card from '../../components/Character/Card';
 
 export default function CharactersList() {
     const [characters, setCharacters] = useState([]);
+    const [results, setResults] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [search, setSearch] = useState('');
+    const searching = !!search.length;
+    const noResults = searching && !results.length;
+    const characterList = searching ? results:characters; 
+
+    const handleSearch = (e) => {
+        setSearch(e.target.value);
+        const searchedCharacters = characters.filter((char) => 
+        char.name.toLowercase().includes(e.target.value.toLowercase().trim()));
+
+        setResults(searchedCharacters);
+    } 
     
-    useEffect(() => {
+  useEffect(() => {
         async function fetchCharacters() {
         const data = await getCharacter();
         console.log(data);
         setCharacters(data);
+        setLoading(false);
     }
     fetchCharacters();
-}, []);
-  return (
+    }, []);
+    return (
       <>
         <h2>Star Wars Characters</h2>
         <div className="chars">
